14d56c87a75b129e5df29cb654ca53ecccaa4285
Alejandro Daneri <alejandrodaneri07@gmail.com>
2020-10-10 13:24:12 -0300

    fix crash on initial page

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.get('/ping',(req,res) =>{
   res.status(200).json("Ping")
 })
 
+app.get('/',(req,res) =>{
+  res.status(200).json("Welcome")
+})
+
+
 const server = app.listen(PORT,()=>{
   console.log("Listen in port:", PORT)
 })
