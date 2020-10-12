782d0728fc85c7b1295ba6b6851841d7b98e7d93
Alejandro Daneri <alejandrodaneri07@gmail.com>
2020-10-11 21:58:36 -0300

    saco CORS

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 require('dotenv').config()
 const express = require('express');
-var cors = require('cors')
 const app = express();
 const routes = require('./src/api/routes')
 
 app.use(express.json())
 
-app.use(cors())
 app.use('/', routes)
 
 const server = app.listen(process.env.PORT,()=>{
